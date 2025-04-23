6dc89c4547553a36afb16f90e1bef2bcc99ae296
Illia Seheda <illias@wix.com>
2025-04-23 15:53:58 +0300

    add textnode and tests for it

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import TextNode, TextType
+
+def main():
+    print(TextNode("Some image", TextType.IMAGE, "https://example.com"))
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,59 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_create_text_node(self):
+        node1 = TextNode("Hello world", TextType.NORMAL)
+        self.assertEqual(node1.text, "Hello world")
+        self.assertEqual(node1.text_type, TextType.NORMAL)
+        self.assertIsNone(node1.url)
+
+        node2 = TextNode("Important", TextType.BOLD)
+        self.assertEqual(node2.text, "Important")
+        self.assertEqual(node2.text_type, TextType.BOLD)
+
+        node3 = TextNode("Click here", TextType.LINK, "https://example.com")
+        self.assertEqual(node3.text, "Click here")
+        self.assertEqual(node3.text_type, TextType.LINK)
+        self.assertEqual(node3.url, "https://example.com")
+
+    def test_equality_comparison(self):
+        node1 = TextNode("Same content", TextType.NORMAL)
+        node2 = TextNode("Same content", TextType.NORMAL)
+        self.assertEqual(node1, node2)
+
+        node3 = TextNode("Different content", TextType.NORMAL)
+        self.assertNotEqual(node1, node3)
+
+        node4 = TextNode("Same content", TextType.BOLD)
+        self.assertNotEqual(node1, node4)
+
+        node5 = TextNode("Link text", TextType.LINK, "https://example.com")
+        node6 = TextNode("Link text", TextType.LINK, "https://another.com")
+        self.assertNotEqual(node5, node6)
+
+    def test_string_representation(self):
+        node1 = TextNode("Test", TextType.NORMAL)
+        self.assertEqual(repr(node1), "TextNode(Test, normal, None)")
+
+        node2 = TextNode("def hello():", TextType.CODE)
+        self.assertEqual(repr(node2), "TextNode(def hello():, code, None)")
+
+        node3 = TextNode(
+            "My Image",
+            TextType.IMAGE,
+            "https://example.com/image.jpg"
+        )
+        self.assertEqual(
+            repr(node3),
+            "TextNode(My Image, image, https://example.com/image.jpg)"
+        )
+
+if __name__ == "__main__":
+    unittest.main()
