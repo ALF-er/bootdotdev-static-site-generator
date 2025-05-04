56ae986e98032514f22b08a2b80aae4eabc639df
Illia Seheda <illias@wix.com>
2025-05-04 15:19:35 +0300

    utility to copy static/ into public/

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_dir_content(src, dst):
+    if not os.path.exists(src):
+        raise Exception("source directory doesn't exist")
+
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+
+    os.mkdir(dst)
+
+    dir_items = os.listdir(src)
+
+    for item in dir_items:
+        item_path = os.path.join(src, item)
+
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, dst)
+        else:
+            copy_dir_content(item_path + "/", os.path.join(dst, item + "/"))
 
 def main():
-    print(TextNode("Some image", TextType.IMAGE, "https://example.com"))
+    copy_dir_content("./static/", "./public/")
 
 main()
