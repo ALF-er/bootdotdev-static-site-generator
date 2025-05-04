db9c596403e11dae074c96d998b6a7563b11fc54
Illia Seheda <illias@wix.com>
2025-05-04 21:03:07 +0300

    add generate_pages_recursive and content for few more pages

---
+++ src/generate_pages_recursive.py
@@ -0,0 +1,15 @@
+import os
+
+from generate_page import generate_page
+
+def generate_pages_recursive(dir_path_content, template_path, dst_dir_path):
+    dir_items = os.listdir(dir_path_content)
+
+    for item in dir_items:
+        item_path = os.path.join(dir_path_content, item)
+        dst_path = os.path.join(dst_dir_path, item)
+
+        if os.path.isfile(item_path) and item[-3:] == ".md":
+            generate_page(item_path, template_path, dst_path[:-3] + ".html")
+        elif os.path.isdir(item_path):
+            generate_pages_recursive(item_path, template_path, dst_path)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from copy_dir_content import copy_dir_content
-from generate_page import generate_page
+from generate_pages_recursive import generate_pages_recursive
 
 def main():
     copy_dir_content("./static/", "./public/")
 
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages_recursive("./content/", "./template.html", "./public/")
 
 main()
